8815fd6ad73aad82319e77e0f7cb3b3c925aaa19
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-11-29 17:33:35 +0545

    update and delete routes created

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
 const router = express.Router();
 
 router.get("/getposts", getPosts);
 router.post("/createpost", createPost);
+router.put("/updatepost", updatePost);
+router.delete("/deletepost", deletePost);
 export default router;

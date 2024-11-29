289420b1cadccab5776733861abde2eeda4f217f
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-11-29 17:06:14 +0545

    Revert "lawfirm backend"
    This reverts commit 29dd5cc51aa24712b9faf5a7a66f40a01c5f61b0.

--- routes/post.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
-const router = express.Router();
-
-router.get("/getposts", getPosts);
-router.post("/createpost", createPost);
-export default router;

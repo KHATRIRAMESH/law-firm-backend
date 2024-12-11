7c14a576e0d00f3da2051dafa35433f108adfd71
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-12-11 16:36:18 +0545

    file uplaod without storing temporarily in the server

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -9,12 +9,13 @@ import path from "path";
 
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/temp')
-  },
+  // destination: function (req, file, cb) {
+  //   cb(null, './public/temp')
+  // },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix+path.extname(file.originalname))
+    // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    // cb(null, file.fieldname + '-' + uniqueSuffix+path.extname(file.originalname))
+    cb(null, file.originalname)
   }
 })
 // console.log(`filename:`)

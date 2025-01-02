eb8e9ecac04e45bce62e4d7ea52b0a527b79b28a
KHATRIRAMESH <khatriramesh972@gmail.com>
2025-01-02 16:10:57 +0545

    unwanted lines were deleted

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,41 +1,18 @@
 import multer from "multer";
 import path from "path";
 
-// const storage = multer.memoryStorage();
-
-// export const upload = multer({
-//   storage
-// });
-
 const storage = multer.memoryStorage({
-destination: function (req, file, cb) {
-  cb(null, '../temp')
-},
-filename: function (req, file, cb) {
-const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-cb(null, file.fieldname + '-' + uniqueSuffix+path.extname(file.originalname))
-cb(null, file.originalname)
-}
+  destination: function (req, file, cb) {
+    cb(null, "../temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+    cb(null, file.originalname);
+  },
+});
 
-
-
-})
-
-export const upload = multer({ storage })
-// console.log(`filename:`)
-
-
-// export const upload = multer({
-//   storage: multer.memoryStorage(),
-//   limits: {
-//     fileSize: 5 * 1024 * 1024, // 5MB file size limit
-//   },
-//   fileFilter: (req, file, cb) => {
-//     const allowedTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
-//     if (allowedTypes.includes(file.mimetype)) {
-//       cb(null, true);
-//     } else {
-//       cb(new Error("Invalid file type. Only images are allowed."), false);
-//     }
-//   },
-// });
+export const upload = multer({ storage });

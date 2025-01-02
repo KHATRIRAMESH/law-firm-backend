8c5cd60e2de80b0c564927da164aaf005067040b
KHATRIRAMESH <khatriramesh972@gmail.com>
2025-01-02 16:09:41 +0545

    index file changed

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
+
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
